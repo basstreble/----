55c4abcbb18226c1dc660bee815a1d2a008d681c
basstreble <854418908@qq.com>
2016-08-28 00:01:47 +0800

    mod update
    mod update

---
+++ 自动分配工具/自动分配工具/PublicMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace auto_distribution
+{
+    class PublicMethods
+    {
+        public static int getRandomNumber(int min, int max)
+        {
+            int start = min;
+            int end = max;
+            Random rd = new Random();
+            return rd.Next(start, end);
+
+        }
+        public static int transferToInt(string text)
+        {
+            int result=0;
+            var i = int.TryParse(text, out result);
+            if (i&&result>-1)
+                return result;
+            else
+                return -1;
+
+        }
+
+    }
+}
--- 自动分配工具/自动分配工具/Tool.cs
+++ 自动分配工具/自动分配工具/Tool.cs
@@ -6,7 +6,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
 namespace auto_distribution
 {
 
@@ -58,60 +57,79 @@ namespace auto_distribution
 
         //输入员工号获取工具
         private void gettool_Click(object sender, EventArgs e)
-        {    
-            var toolGet = stuffUsingTool.First(p => p.stuff == int.Parse(StuffNo.Text));
-            if (stuffAndToolOnWork.Where(p=>p.stuff==toolGet.stuff).Count()>0)
+        {
+            var stuffGet = PublicMethods.transferToInt(StuffNo.Text);
+            if (stuffGet != -1)
             {
-                MessageBox.Show("您已经处于工作状态！");
-                return;
+                var toolGet = stuffUsingTool.First(p => p.stuff == PublicMethods.transferToInt(StuffNo.Text));
+                if (stuffAndToolOnWork.Where(p => p.stuff == toolGet.stuff).Any())
+                {
+                    MessageBox.Show("您已经处于工作状态！");
+                    return;
+                }
+                if (!toolWaiting.Any())
+                {
+                    var rd = PublicMethods.getRandomNumber(0, 199);
+                    var stuffAndTool = stuffAndToolOnWork[rd];
+                    stuffAndToolOnWork.RemoveAt(rd);
+                    toolWaiting.Remove(stuffAndTool.using_tool);
+                    MessageBox.Show(stuffAndTool.stuff + "号工人因满员而强制下班！");
 
-            }
-            if (toolWaiting.Contains(toolGet.using_tool))
-            {
-                MessageBox.Show(toolGet.using_tool + "号工具给你使用");
-                toolWaiting.Remove(toolGet.using_tool);
-                var temp = new Stuff_using_tool();
-                temp.stuff = int.Parse(StuffNo.Text);
-                temp.using_tool = toolGet.using_tool;
-                stuffAndToolOnWork.Add(temp);
+                }
+                if (toolWaiting.Contains(toolGet.using_tool))
+                {
+                    MessageBox.Show(toolGet.using_tool + "号工具给你使用");
+                    toolWaiting.Remove(toolGet.using_tool);
+                    var temp = new Stuff_using_tool();
+                    temp.stuff = stuffGet;
+                    temp.using_tool = toolGet.using_tool;
+                    stuffAndToolOnWork.Add(temp);
+                }
+                else
+                {
+                    //工人熟悉工具在使用，就分配一个默认使用它的人数最少的工具，并且将此工人的默认使用工具改成此工具             
+                    var toolUseList = stuffUsingTool.GroupBy(p => p.using_tool).OrderBy(p => p.Count()).Select(p => new { tool = p.Key, useNumber = p.Count() }).ToList();
+                    for (int i = 0; i < 200; i++)
+                    {
+                        //如果此工具空闲，则交付工人使用
+                        if (toolWaiting.Contains(toolUseList[i].tool))
+                        {
+                            var temp = new Stuff_using_tool();
+                            temp.stuff = stuffGet;
+                            temp.using_tool = toolUseList[i].tool;
+                            stuffAndToolOnWork.Add(temp);//添加工作中的工人工具编号
+                            toolWaiting.Remove(toolUseList[i].tool);//移除该空闲工具
+                            stuffUsingTool.Remove(toolGet);//移除旧的工人熟练使用的工具号
+                            //添加新的工人熟练使用的工具号
+                            stuffUsingTool.Insert(stuffGet, new Stuff_using_tool { stuff = stuffGet, using_tool = toolUseList[i].tool });
+                            MessageBox.Show(toolUseList[i].tool + "号工具给你临时使用，请熟悉此工具");
+                            break;
+                        }
+                    }
+
+                }
             }
             else
-            {
-                //工人熟悉工具在使用，就分配一个默认使用它的人数最少的工具，并且将此工人的默认使用工具改成此工具             
-                var toolUseList = stuffUsingTool.GroupBy(p => p.using_tool).OrderBy(p=>p.Count()).Select(p=>new{tool=p.Key,useNumber=p.Count() }).ToList();
-                for (int i = 0; i < 200; i++)
-                {
-                    //如果此工具空闲，则交付工人使用
-                   if(toolWaiting.Contains(toolUseList[i].tool))
-                   {
-                       var temp = new Stuff_using_tool();
-                       temp.stuff = int.Parse(StuffNo.Text);
-                       temp.using_tool = toolUseList[i].tool;
-                       stuffAndToolOnWork.Add(temp);//添加工作中的工人工具编号
-                       toolWaiting.Remove(toolUseList[i].tool);//移除该空闲工具
-                       stuffUsingTool.Remove(toolGet);//移除旧的工人熟练使用的工具号
-                       //添加新的工人熟练使用的工具号
-                       stuffUsingTool.Insert(int.Parse(StuffNo.Text),new Stuff_using_tool { stuff = int.Parse(StuffNo.Text), using_tool = toolUseList[i].tool });
-                       MessageBox.Show(toolUseList[i].tool + "号工具给你临时使用，请熟悉此工具");
-                       break;
-                   }
-                }
-              
-            }
+                MessageBox.Show("员工号不正确！");
         }
         //
         private void returntool_Click(object sender, EventArgs e)
         {
-            var toolNo = int.Parse(ToolNo.Text);
-            var stuffNo = stuffAndToolOnWork.FirstOrDefault(p => p.using_tool == toolNo);
-            if (stuffNo != null)
+            var toolGet = PublicMethods.transferToInt(ToolNo.Text);
+            if (toolGet > -1)
             {
-                toolWaiting.Add(toolNo);
-                stuffAndToolOnWork.Remove(stuffAndToolOnWork.First(p => p.using_tool == toolNo));
-                MessageBox.Show(stuffNo.stuff + "号工人下班，" + toolNo + "号工具已归还");
+                var stuffNo = stuffAndToolOnWork.FirstOrDefault(p => p.using_tool == toolGet);
+                if (stuffNo != null)
+                {
+                    toolWaiting.Add(toolGet);
+                    stuffAndToolOnWork.Remove(stuffAndToolOnWork.First(p => p.using_tool == toolGet));
+                    MessageBox.Show(stuffNo.stuff + "号工人下班，" + toolGet + "号工具已归还");
+                }
+                else
+                    MessageBox.Show("请勿重复下班！");
             }
             else
-                MessageBox.Show("请勿重复下班！");
+                MessageBox.Show("工具号不正确！");
 
         }
 
